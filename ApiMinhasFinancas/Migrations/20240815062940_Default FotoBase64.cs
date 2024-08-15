3c6b826255f77d3f98bfeba49d6d2aa4c71378cf
Lucas Ferreira <lucas_81303518@live.com>
2024-08-15 04:29:50 -0300

    Valor default para FotoBase64, pois nao é no cadastro do usuario que se passa ele.

---
+++ ApiMinhasFinancas/Migrations/20240815062940_Default FotoBase64.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiMinhasFinancas.Migrations
+{
+    public partial class DefaultFotoBase64 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FotoBase64",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "FotoBase64",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldDefaultValue: "");
+        }
+    }
+}
